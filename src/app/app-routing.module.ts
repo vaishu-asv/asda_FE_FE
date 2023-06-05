48f54edc2a1d620f80dbed91f70d6cf20397408e
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 17:44:12 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
+
+  { path: 'modules', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule)},
 ];
 
 @NgModule({
